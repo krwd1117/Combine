08267d6edce1f67ae3e52e2c687ffe64a44e9f2e
Wan <krwd1117@gmail.com>
2022-07-12 16:58:53 +0900

    Operator_Append

--- Combine-playground.playground/Pages/Operator_Append.xcplaygroundpage/Contents.swift
+++ Combine-playground.playground/Pages/Operator_Append.xcplaygroundpage/Contents.swift
@@ -1,7 +1,23 @@
-//: [Previous](@previous)
-
+import Combine
 import Foundation
 
-var greeting = "Hello, playground"
+// MARK: - Append(Sequence)
 
-//: [Next](@next)
+// 1. 값을 방출하는 publisher
+let publisher = [3, 4].publisher
+
+// 2. append로 값을 붙여줌
+publisher
+    .append([5, 6])
+    .sink(receiveValue: {print($0)})
+
+// MARK: - Append(Publisher)
+
+// 1. 두개의 publisher를 생성
+let publisher2 = [1, 2].publisher
+let publisher3 = [3, 4].publisher
+
+// 2. publisher3을 publisher2에 추가
+publisher2
+    .append(publisher3)
+    .sink(receiveValue: {print($0)})
